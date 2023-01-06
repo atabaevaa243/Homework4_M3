8a7c773cbf53f88ab3f3ff271d5aed6b498705eb
Aziza <atabaevaa243@gmail.com>
2023-01-06 23:32:45 +0600

    Initial Commit

---
+++ Homework4_M3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework4_M3
+//
+//  Created by mac on 6/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
